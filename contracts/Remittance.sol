f838bac5e32efa48a655e8976e198e6e6ee6ac63
imalysh <imalysh@luxoft.com>
2018-06-21 13:35:07 +0300

    Change function to transfer and define receiver

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -6,12 +6,14 @@ contract Remittance is Own {
   
   bytes32 public paymentHash;
   uint public  deadline;
+  address public receiver;
 
   event LogPayment(address receiver, uint amount);
 
-  function Remittance(bytes32 _paymentHash, uint _deadline) payable public {
+  function Remittance(bytes32 _paymentHash, uint _deadline, address _receiver) payable public {
     paymentHash = _paymentHash;
     deadline = _deadline;
+    receiver = _receiver;
   }
 
   modifier hasMoney()
@@ -26,7 +28,7 @@ contract Remittance is Own {
   returns(bool success)
   {
       require(block.number > deadline);
-      return msg.sender.call.value(address(this).balance)();
+      return msg.sender.transfer(address(this).balance)();
   }
 
   function receiveMoney(string pass1, string pass2) 
@@ -37,8 +39,10 @@ contract Remittance is Own {
     require(block.number <= deadline);
     require(keccak256(pass1, pass2) == paymentHash);
 
+    require(msg.sender == receiver);
+
     LogPayment(msg.sender, address(this).balance);
-    return msg.sender.call.value(address(this).balance)();     
+    return msg.sender.transfer(address(this).balance)();
   }
 
 }
